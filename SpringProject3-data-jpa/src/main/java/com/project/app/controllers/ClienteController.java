3ab00780014138302d82538816ae0df59196e63a
SMontoya <SMontoya@192.168.1.21>
2018-07-24 18:28:33 -0500

    continuacion del curso - video 58 - Imagenes

--- SpringProject3-data-jpa/src/main/java/com/project/app/controllers/ClienteController.java
+++ SpringProject3-data-jpa/src/main/java/com/project/app/controllers/ClienteController.java
@@ -1,5 +1,9 @@
 package com.project.app.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.app.models.entity.Cliente;
@@ -36,14 +41,15 @@ public class ClienteController {
 		// PageRequest Deprecrated Pageable pr = new PageRequest(page, 5);
 		/* SpringBoot 5 PageRequest */
 		Pageable pr = PageRequest.of(page, 5);
-		
+
 		Page<Cliente> listadoClientesPaginado = clienteService.findAll(pr);
 		PageRender<Cliente> pageRender = new PageRender<>("/listar", listadoClientesPaginado);
 		m.addAttribute("title", "Listado de Clientes");
 		m.addAttribute("clients", listadoClientesPaginado);
 		m.addAttribute("page", pageRender);
-		// m.addAttribute("clients", clienteService.findAll());			listar todos los clientes
-		
+		// m.addAttribute("clients", clienteService.findAll()); listar todos los
+		// clientes
+
 		return "listar";
 	}
 
@@ -58,12 +64,25 @@ public class ClienteController {
 	}
 
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cl, BindingResult result, Model m, RedirectAttributes flash,
-			SessionStatus stat) {
+	public String guardar(@Valid Cliente cl, BindingResult result, Model m, @RequestParam("file") MultipartFile image,
+			RedirectAttributes flash, SessionStatus stat) {
 
 		if (result.hasErrors()) {
 			m.addAttribute("title", "Formulario de Cliente");
 			return "form";
+		}
+
+		if (!image.isEmpty()) {
+			Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
+			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+
+			try {
+				byte[] bytes = image.getBytes();
+				Path rutaCompleta = Paths.get(rootPath + "//" + image.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		String mensajeFlash = (cl.getId() != null) ? "El Cliente se edito correctamente"
 				: "Cliente creado correctamente";
